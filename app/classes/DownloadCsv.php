c6766fe27f2eb9fa05a7b1c1851df595838d9aae
Atmena <tireau.steph@gmail.com>
2023-10-06 16:49:16 +0200

    fix: Mise en forme en POO

--- app/classes/DownloadCsv.php
+++ app/classes/DownloadCsv.php
@@ -1,23 +1,28 @@
 <?php
 require_once('CsvFilter.php');
 
-if (isset($_POST['applyFilters']) && isset($_POST['csvFileName'])) {
-    $csvFileName = $_POST['csvFileName'];
-    $csvContent = file_get_contents('../uploads/' . $csvFileName);
+class DownloadCsv {
+    public function processDownload($csvFileName) {
+        if (isset($_POST['applyFilters']) && isset($csvFileName)) {
+            $csvContent = file_get_contents('../uploads/' . $csvFileName);
 
-    // Appliquer les filtres
-    $filteredContent = CsvFilter::applyFilters($csvContent);
+            // Appliquer les filtres
+            $filteredContent = CsvFilter::applyFilters($csvContent);
 
-    // Modifiez la ligne pour créer le nom du fichier de sortie
-    $outputFileName = '../uploads/' . pathinfo($csvFileName, PATHINFO_FILENAME) . 'Update.csv';
+            // Modifiez la ligne pour créer le nom du fichier de sortie
+            $outputFileName = '../uploads/' . pathinfo($csvFileName, PATHINFO_FILENAME) . 'Update.csv';
 
-    // Enregistrer le fichier filtré
-    file_put_contents($outputFileName, $filteredContent);
+            // Enregistrer le fichier filtré
+            file_put_contents($outputFileName, $filteredContent);
 
-    // Ne pas télécharger automatiquement, mais renvoyer le nom du fichier généré
-    echo json_encode(array('success' => true, 'outputFileName' => basename($outputFileName)));
+            // Télécharger le fichier généré
+            header('Content-Type: application/csv');
+            header('Content-Disposition: attachment; filename="' . basename($outputFileName) . '"');
+            readfile($outputFileName);
+            exit;
+        }
+        
+        return array('success' => false, 'error' => 'Les filtres n\'ont pas été appliqués.');
+    }
 }
-
-// Rediriger vers la page d'origine (index.php)
-header('Location: ../index.php');
 ?>
