a3d101ffb3133c41b34497b961dc29e2a15e508a
Atmena <tireau.steph@gmail.com>
2023-10-06 14:01:17 +0200

    feat: Mise en place d'un générateur de csv simple

---
+++ app/csvGenerator/csvGenerator.php
@@ -0,0 +1,41 @@
+<?php
+if (isset($_POST['generateCsv'])) {
+    // Lire les fichiers texte de noms et prénoms
+    $surname = file('nom.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $name = file('prenom.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $city = ['Dijon', 'Besançon', 'Belfort', 'Chalon-sur-Saône', 'Nevers', 'Auxerre', 'Mâcon', 'Montbéliard', 'Sens', 'Dole', ];
+
+    // Créer le contenu du fichier CSV avec 200 lignes supplémentaires
+    $csvContent = "Nom,Prénom,Âge,Ville\n";
+    for ($i = 0; $i < 200; $i++) {
+        $randomSurname = $surname[array_rand($surname)];
+        $randomName = $name[array_rand($name)];
+        $randomAge = rand(18, 70);
+        $randomCity = $city[array_rand($city)];
+        $csvContent .= "$randomSurname,$randomName,$randomAge,$randomCity\n";
+    }
+
+    // En-têtes HTTP pour le téléchargement du fichier
+    header('Content-Type: application/csv');
+    header('Content-Disposition: attachment; filename="identity.csv"');
+
+    // Sortie du contenu du fichier CSV
+    echo $csvContent;
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Génération de fichier CSV</title>
+</head>
+<body>
+    <h1>Génération de fichier CSV</h1>
+    <form method="post" action="">
+        <input type="submit" name="generateCsv" value="Générer le fichier CSV">
+    </form>
+</body>
+</html>
